8fad28237def94df146d87c21a5fda8f9fc8293f
Gabriel Takahashi <taakahashi@gmail.com>
2022-11-27 21:22:26 -0300

    Adicionando spy no teste de listagem das categorias

--- tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Api\CategoryController;
 use Core\UseCase\Category\ListCategoriesUseCase;
 use Core\UseCase\DTO\Category\ListCategories\ListCategoriesOutputDTO;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 use Mockery;
 use Tests\TestCase;
 
@@ -29,5 +28,14 @@ class CategoryControllerTest extends TestCase
 
         static::assertIsObject($response->resource);
         static::assertArrayHasKey('meta', $response->additional);
+
+        //Spies
+        $mockUseCase = Mockery::spy(ListCategoriesUseCase::class);
+        $mockUseCase->shouldReceive('execute')->andReturn($mockOutputDTO);
+
+        $controller->index($mockRequest, $mockUseCase);
+
+        $mockUseCase->shouldHaveReceived('execute');
+        //Spies
     }
 }
